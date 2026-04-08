b75047aa4f25120d5861d8836c29246048566c84
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2026-04-08 16:10:49 +0200

    declare extraction methods –– Only Tom Cruise knows why.

--- apps/sidecar/src/sidecar/extractors/__init__.py
+++ apps/sidecar/src/sidecar/extractors/__init__.py
@@ -1,4 +1,7 @@
 from .plain_text import extract_plain_text_document
-from .pdf import
+from .pdf import extract_pdf_document
+from .microsoft import extract_office_document
 
-__all__ = ["extract_plain_text_document", "extract_pdf_document"]
+__all__ = ["extract_plain_text_document",
+           "extract_pdf_document",
+           "extract_office_document"]
