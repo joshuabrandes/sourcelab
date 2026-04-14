7bc8d733b96ea948ec2e85fe30be7fc480e55eea
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2026-04-14 10:35:28 +0200

    more package method definitions for content extractors...

--- apps/sidecar/src/sidecar/extractors/__init__.py
+++ apps/sidecar/src/sidecar/extractors/__init__.py
@@ -1,10 +1,14 @@
-from .plain_text import extract_plain_text_document
-from .pdf import extract_pdf_document
-from .microsoft import extract_office_document
 from .html_url import extract_html_document, extract_url_document
+from .image import extract_image_document
+from .microsoft import extract_office_document
+from .pdf import extract_pdf_document
+from .plain_text import extract_plain_text_document
+from .youtube import extract_youtube_document
 
 __all__ = ["extract_plain_text_document",
            "extract_pdf_document",
            "extract_office_document",
            "extract_html_document",
-           "extract_url_document"]
+           "extract_url_document",
+           "extract_image_document",
+           "extract_youtube_document"]
